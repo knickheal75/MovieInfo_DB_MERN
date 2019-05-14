fd1a0ba021cb11a8158535ec7ec01934cb39422c
knickheal75 <blitzkrieg0705@gmail.com>
2019-05-15 00:00:00 +0530

    initial boilerplate setup and API configuration movieDB

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_KEY = '9303d68c1791b5e654c0520435cbc04f';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_URL = 'https://image/tmdb.org/t/p/'
---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports={
+    mongoURI:'mongodb+srv://nikhil75:blitz0705@cluster0-u9yfd.mongodb.net/test?retryWrites=true&w=majority'
+}
